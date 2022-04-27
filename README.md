8166ed964e86ff212227c8a2d8cc2abe0adcbc21
Irving <irvingcm@example.com>
2022-04-27 16:55:30 -0500

    yo

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+Run git clone <this project>
+Run composer install.
+Run cp .env.example .env.
+Run php artisan key:generate.
+Run php artisan migrate. //If you have DB
+Run php artisan serv
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '7380ea151e444282d72e0fcc91a50e237fa5762c',
+        'reference' => 'a1eb34abe60d096def176aead02a884deccba078',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -379,7 +379,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '7380ea151e444282d72e0fcc91a50e237fa5762c',
+            'reference' => 'a1eb34abe60d096def176aead02a884deccba078',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
@@ -736,8 +736,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
